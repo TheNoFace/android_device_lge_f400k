db3a4f657af1c19290d1d802b885d87da3c20f42
Joonhyuk Song <fprhqkrtk303@naver.com>
2015-11-27 00:16:40 +0900

    Change F400K into F400

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/lge/f400k/full_f400k.mk)
+$(call inherit-product, device/lge/f400/full_f400.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -6,6 +6,6 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_f400k
+PRODUCT_NAME := cm_f400
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_DEVICE="g3" PRODUCT_NAME="g3_kt_kr" BUILD_FINGERPRINT="lge/g3_kt_kr/g3:4.4.2/KVT49L.F400K10t/F400K10t.1412647512:user/release-keys" PRIVATE_BUILD_DESC="g3_kt_kr-user 4.4.2 KVT49L F400K10t.1412647512 release-keys"
